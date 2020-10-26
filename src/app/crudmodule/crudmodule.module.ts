25973f056cc86e45b98c0b154c603b2ddd984e2b
nishanthr93 <48874622+nishanthr93@users.noreply.github.com>
2020-10-26 20:48:18 +0530

    delete the crud component

--- src/app/crudmodule/crudmodule.module.ts
+++ src/app/crudmodule/crudmodule.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CurdmoduleComponentComponent } from './curdmodule-component/curdmodule-component.component';
 
 
 
 @NgModule({
-  declarations: [CurdmoduleComponentComponent],
+  declarations: [],
   imports: [
     CommonModule
   ]
