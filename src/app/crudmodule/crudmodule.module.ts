34596e552caf245b4da2482f48b0f0cabb749828
nishanthr93 <48874622+nishanthr93@users.noreply.github.com>
2020-11-01 21:48:30 +0530

    ng mofule

--- src/app/crudmodule/crudmodule.module.ts
+++ src/app/crudmodule/crudmodule.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CurdmoduleComponent } from './curdmodule/curdmodule.component';
-
-
-
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [CurdmoduleComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   exports:[
     CurdmoduleComponent
--- src/app/crudmodule/curdmodule/curdmodule.component.ts
+++ src/app/crudmodule/curdmodule/curdmodule.component.ts
@@ -5,11 +5,17 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './curdmodule.component.html',
   styleUrls: ['./curdmodule.component.css']
 })
+
 export class CurdmoduleComponent implements OnInit {
 
+  public input_name : string;
+  
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  classname = "test";
+  echo(event){
+    this.input_name = event.target.value;
+  }
 }
